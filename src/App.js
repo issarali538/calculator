5d3a6afc589e87a6fc17eb4e1ae38c36fb9329c4
issarali538 <you@example.com>
2023-05-10 23:01:29 +0500

    commit 10-april

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React, { useState } from 'react';
-import Theme from './components/ThemContext';
-import ThemSwitcher from './components/ThemeSwitcher';
-import CheckUserContext from './components/Credentials/CheckUserContext';
-import TrueUser from './components/TrueUser';
-import UseMemoComp from './components/UseMemo';
 import CalculatorComp from './components/Calculator/CalculatorComponenet';
 
 const App = function () {
   return (
     <div>
-      <CheckUserContext>
-        <Theme>
-          <ThemSwitcher />
-          <TrueUser/>
-      {/* <UseMemoComp /> */}
-        </Theme>
-      </CheckUserContext>/
-      <hr />
       <CalculatorComp />
     </div>
   )
--- src/components/Credentials/UserContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-const user = createContext();
-export default user;
